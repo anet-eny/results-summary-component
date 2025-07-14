55f88accee3d72861b7ebb8a87f9f0e935bc8c01
anet-eny <anet-eny@gmail.com>
2025-07-14 18:36:45 +0200

    Install react-icons and create iconMap in data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,15 @@
+import { FaBolt } from "react-icons/fa";
+import { MdMemory } from "react-icons/md";
+import { RiChat3Line } from "react-icons/ri";
+import { IoEyeSharp } from "react-icons/io5";
+
+export const iconMap = {
+  Reaction: FaBolt,
+  Memory: MdMemory,
+  Verbal: RiChat3Line,
+  Visual: IoEyeSharp,
+};
+
 export const resultsData = [
   {
     category: "Reaction",
