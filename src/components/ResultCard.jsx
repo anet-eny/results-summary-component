287077e5771960299dd7f36d9f04dde94a0ebff0
anet-eny <anet-eny@gmail.com>
2025-07-14 19:03:51 +0200

    Create structure of the ResultCard component

---
+++ src/components/ResultCard.jsx
@@ -0,0 +1,13 @@
+import ResultScore from "./ResultScore";
+import ResultSummary from "./ResultSummary";
+
+function ResultCard() {
+  return (
+    <div>
+      <ResultScore />
+      <ResultSummary />
+    </div>
+  );
+}
+
+export default ResultCard;
