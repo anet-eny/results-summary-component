2d94d692579b532d63a94722fd2b51d2e585fa84
anet-eny <anet-eny@gmail.com>
2025-07-14 14:55:47 +0200

    Initial Vite + React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="p-6 bg-blue-500 text-white rounded-lg">
+        <h1 className="text-2xl font-bold">Tailwind funguje 🎉</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
