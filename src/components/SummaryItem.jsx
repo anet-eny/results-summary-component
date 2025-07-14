204f537de443730ff736af99dc84b4aea3653f4d
anet-eny <anet-eny@gmail.com>
2025-07-14 19:00:13 +0200

    Create structure of the SummaryItem component

--- src/components/SummaryItem.jsx
+++ src/components/SummaryItem.jsx
@@ -1,3 +1,13 @@
-function SummaryItem() {}
+function SummaryItem({ category, score, Icon }) {
+  return (
+    <div>
+      <div>
+        <Icon />
+        <span>{category}</span>
+      </div>
+      <span>{score} / 100</span>
+    </div>
+  );
+}
 
 export default SummaryItem;
