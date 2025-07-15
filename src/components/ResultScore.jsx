831baf3646cb604532b3f0bcec75249e2726be88
anet-eny <anet-eny@gmail.com>
2025-07-15 21:04:58 +0200

    Create structure of the ResultScore component

--- src/components/ResultScore.jsx
+++ src/components/ResultScore.jsx
@@ -1,5 +1,16 @@
 function ResultScore() {
-  return <div></div>;
+  return (
+    <div>
+      <h3>Your result</h3>
+      <div>
+        76 <span>of 100</span>
+      </div>
+      <h2>Great</h2>
+      <p>
+        You scored higher than 65% of the people who have taken these tests.
+      </p>
+    </div>
+  );
 }
 
 export default ResultScore;
