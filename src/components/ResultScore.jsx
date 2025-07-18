3bce8bb8b2d2b80e04e615baee5147616213aabe
anet-eny <anet-eny@gmail.com>
2025-07-18 14:25:33 +0200

    Update text sizes based on Figma

--- src/components/ResultScore.jsx
+++ src/components/ResultScore.jsx
@@ -1,13 +1,13 @@
 function ResultScore() {
   return (
     <div className="bg-gradient-to-b from-violet-500 to-indigo-600 text-white flex flex-col items-center justify-center gap-4 p-6 md:w-1/2 rounded-b-2xl md:rounded-2xl">
-      <h3 className="font-semibold">Your result</h3>
+      <h3 className="font-semibold text-2xl">Your result</h3>
       <div className="bg-gradient-to-b from-violet-700 to-indigo-500 w-24 h-24 rounded-full flex flex-col items-center justify-center gap-1 ">
-        <h1 className="text-white font-bold text-4xl">76</h1>
-        <span className="text-xs font-normal">of 100</span>
+        <h1 className="text-white font-bold text-7xl">76</h1>
+        <span className="text-lg font-normal">of 100</span>
       </div>
-      <h2 className="text-lg font-semibold">Great</h2>
-      <p className="text-center text-sm px-4">
+      <h2 className="text-3xl font-semibold">Great</h2>
+      <p className="text-center text-lg px-4">
         You scored higher than 65% of the people who have taken these tests.
       </p>
     </div>
--- src/components/SummaryItem.jsx
+++ src/components/SummaryItem.jsx
@@ -1,13 +1,13 @@
 function SummaryItem({ category, score, Icon }) {
   return (
-    <div className="flex items-center justify-between bg-gray-100 rounded p-3">
+    <div className="flex items-center justify-between bg-gray-100 rounded-xl p-3">
       <div className="flex items-center gap-2">
         <Icon className="text-xl" />
-        <span className="text-sm">{category}</span>
+        <span className="text-lg">{category}</span>
       </div>
       <div>
-        <span className="text-gray-700 font-bold">{score}</span>
-        <span className="text-gray-400 font-bold"> / 100</span>
+        <span className="text-gray-700 font-bold text-lg">{score}</span>
+        <span className="text-gray-400 font-bold text-lg"> / 100</span>
       </div>
     </div>
   );
